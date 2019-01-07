ae2ea09c9990d140ed6b906999ff7e44c9059b76
Yutaro <yutaro.iiyama@cern.ch>
2019-01-07 10:29:58 +0100

    modernized

--- bin/read_converted.py
+++ bin/read_converted.py
@@ -7,40 +7,48 @@ except ImportError:
 
 import sys
 import os
-from argparse import ArgumentParser
-
-parser = ArgumentParser(description='Read converted records')
-parser.add_argument('converter', help="Converter class")
-parser.add_argument('input', help="Path to input tfrecords")
-parser.add_argument('nevents', nargs = '?', default = 1, type = int, help = 'Number of events to read')
-args = parser.parse_args()
-
-sys.argv = []
+import importlib
 
 import numpy as np
 import tensorflow as tf
 
-import conversion
+def read_converted(converter, input, nevents):
+    modname = converter[:converter.find('.')]
+    convmod = importlib.import_module('conversion.' + modname)
+    converter = getattr(convmod, converter[converter.find('.') + 1:])()
+    
+    feature_mapping = converter.feature_mapping()
+    feature_mapping['labels_one_hot'] = tf.FixedLenFeature((2,), tf.int64)
+    #feature_mapping['labels_one_hot'] = tf.FixedLenFeature((6,), tf.int64)
+    #feature_mapping['labels_one_hot'] = tf.VarLenFeature(tf.int64)
+    
+    def parse_one(example_proto):
+        parsed = tf.parse_single_example(example_proto, feature_mapping)
+        return parsed
+    
+    dataset = tf.data.TFRecordDataset([input], compression_type = 'GZIP') \
+                     .map(parse_one) \
+                     .batch(nevents)
+    
+    iterator = dataset.make_one_shot_iterator()
+    next_element = iterator.get_next()
+    
+    with tf.Session() as sess:
+        example, = sess.run([next_element])
+    
+        return converter, example
 
-converter = getattr(conversion, args.converter)()
-
-feature_mapping = converter.feature_mapping()
-feature_mapping['labels_one_hot'] = tf.FixedLenFeature((2,), tf.int64)
-#feature_mapping['labels_one_hot'] = tf.FixedLenFeature((6,), tf.int64)
-#feature_mapping['labels_one_hot'] = tf.VarLenFeature(tf.int64)
-
-def parse_one(example_proto):
-    parsed = tf.parse_single_example(example_proto, feature_mapping)
-    return parsed
-
-dataset = tf.data.TFRecordDataset([args.input], compression_type = 'GZIP') \
-                 .map(parse_one) \
-                 .batch(args.nevents)
-
-iterator = dataset.make_one_shot_iterator()
-next_element = iterator.get_next()
-
-with tf.Session() as sess:
-    example, = sess.run([next_element])
-
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser(description='Read converted records')
+    parser.add_argument('converter', help="Converter class")
+    parser.add_argument('input', help="Path to input tfrecords")
+    parser.add_argument('nevents', nargs = '?', default = 1, type = int, help = 'Number of events to read')
+    args = parser.parse_args()
+    
+    sys.argv = []
+    
+    converter, example = read_converted(args.converter, args.input, args.nevents)
+    
     converter.print_example(example)
