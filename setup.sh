443d59e4f381fbfcf7f7651b33e9294c17af0ac4
Yutaro <yutaro.iiyama@cern.ch>
2019-01-15 16:20:44 +0100

    activate in the setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 THISDIR=$(cd $(dirname $BASH_SOURCE); pwd)
+
+source activate py36tensorflow
+
 export PYTHONPATH
 echo $PYTHONPATH | grep -q "$THISDIR/lib:" || PYTHONPATH=$THISDIR/lib:$PYTHONPATH
 export PREFERRED_GPUS=4,5,6,7
