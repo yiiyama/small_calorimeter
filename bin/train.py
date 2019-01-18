3caca20709ea012f5fe0c7bbba1613c8334c1690
Yutaro <yutaro.iiyama@cern.ch>
2019-01-18 16:53:29 +0100

    run_for option

--- bin/train.py
+++ bin/train.py
@@ -14,6 +14,7 @@ parser.add_argument('config', help="Config section within the config file")
 parser.add_argument('--debug', '-D', action = 'store_true', dest = 'debug', help='Run the debug printouts')
 parser.add_argument('--evaluate', '-E', action = 'store_true', dest = 'evaluate', help='Run the evaluation from saved training results')
 parser.add_argument('--continue', '-C', action = 'store_true', dest = 'use_saved', help='Continue training from saved data')
+parser.add_argument('--run-for', '-i', dest = 'run_for', type = int, default = 100, help='Number of evaluation iterations')
 
 args = parser.parse_args()
 
@@ -22,6 +23,6 @@ trainer = Trainer(args.input, args.config)
 if args.debug:
     trainer.debug(trained = args.use_saved)
 elif args.evaluate:
-    trainer.evaluate()
+    trainer.evaluate(args.run_for)
 else:
     trainer.train(from_scratch = (not args.use_saved))
